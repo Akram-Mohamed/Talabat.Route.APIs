6266c66a0514b5f7fc5093b7c94338ebadfd3242
Akram Mohamed <akrammohamed508@gmail.com>
2024-04-11 20:24:12 +0200

    Part 09 DbContext, ProductConfigurations, Migrations

--- Talabat.Repositries/Data/StoreContext.cs
+++ Talabat.Repositries/Data/StoreContext.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Talabat.Core.Entities;
 
 namespace Talabat.Repositries.Data
 {
@@ -15,8 +17,16 @@ namespace Talabat.Repositries.Data
 
 		}
 
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-			=> optionsBuilder.UseSqlServer("connectionString");
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
-	}
+		}
+
+		public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+
+    }
 }
