181df1cf91674c2449a04012851d1ef47cb499d7
Akram Mohamed <akrammohamed508@gmail.com>
2024-04-11 18:50:22 +0200

    Part 08 Product Module - Entities

---
+++ Talabat.Core/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+	public class Product : BaseEntitiy
+	{
+
+
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string PictureUrl { get; set; }
+		public decimal Price { get; set; }
+
+		//[ForeignKey(nameof (Product.Brand))]
+		public int BrandId { get; set; } //Forieng Key Column=> Product Brand
+		//[InverseProperty (nameof (ProductBrand. Products))]
+		public ProductBrand Brand { get; set; }//Navigation Property [ONE]
+
+		//[ForeignKey(nameof (Product.Category))]
+		public int CategoryId { get; set; } //Forieng Key Column=> Product Category
+		public ProductCategory Category { get; set; }//Navigation Property [ONE]
+
+
+
+
+
+
+
+	}
+}
---
+++ Talabat.Repositries/Data/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Repositries.Data
+{
+	public class StoreContext : DbContext
+	{
+		public StoreContext(DbContextOptions<StoreContext> dbContextOptions)
+			:base(dbContextOptions)
+		{
+
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			=> optionsBuilder.UseSqlServer("connectionString");
+
+	}
+}
