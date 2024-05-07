55bd2c7a4bf6b94ff92276198cc52d3fe8fdc358
Akram Mohamed <akrammohamed508@gmail.com>
2024-05-07 14:59:54 +0300

    Part 03 Login Endpoint

---
+++ Talabat.Route.APIs/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Core.Entities.Identity;
+using Talabat.Route.APIs.DTOS;
+using Talabat.Route.APIs.Errors;
+
+namespace Talabat.Route.APIs.Controllers
+{
+   
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IAuthenticationService _authService;
+        private readonly IMapper _mapper;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IAuthenticationService authService, IMapper mapper)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _authService = authService;
+            _mapper = mapper;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDTO>> Login(LoginDTO model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user is null) return Unauthorized(new ApiResponse(401, "Invalid Login"));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401, "Invalid Login"));
+
+            return Ok(new UserDTO()
+            {
+                DisplayName = user.DisplayName,
+                Email = model.Email,
+                //Token = await _authService.CreateTokenAsync(user, _userManager)
+            }); ;
+        }
+
+
+
+    }
+}
